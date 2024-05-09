38082ca7732d36ae22dbcc7df23abd439326f512
tsolmonbat-batbold <t.batbold@students.htl-leonding.ac.at>
2024-05-09 19:42:41 +0200

    changes

---
+++ frontend/src/Components/Userprofile/User.tsx
@@ -0,0 +1,17 @@
+
+export default function User(){
+    return(
+        <>
+            <img
+                className="inline-block h-max w-max rounded-full ring-2 ring-white"
+                src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+            />
+            <p>username</p>
+            <p>profile</p>
+            <p>score</p>
+            <p className="bg-amber-500">temp</p>
+
+        </>
+    )
+}
