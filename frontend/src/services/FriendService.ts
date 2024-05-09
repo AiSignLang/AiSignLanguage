a717c207e85623bed7831845b075dc7f80146322
tsolmonbat-batbold <t.batbold@students.htl-leonding.ac.at>
2024-05-09 19:03:20 +0200

    changes

--- frontend/src/services/FriendService.ts
+++ frontend/src/services/FriendService.ts
@@ -1,8 +1,37 @@
 class FriendService{
 
     public async getFriends(){
-        const response = await fetch('http://localhost:3000/friends');
-        return response.json();
+        //const response = await fetch('http://localhost:3000/friends');
+        //
+        // return response.json();
+
+        return [
+            {
+                id: 1,
+                name: 'John Doe',
+                score: 100,
+            },
+            {
+                id: 2,
+                name: 'Jane Smith',
+                score: 120,
+            },
+            {
+                id: 3,
+                name: 'Bob Johnson',
+                score: 80,
+            },
+            {
+                id: 4,
+                name: 'Alice Williams',
+                score: 150,
+            },
+            {
+                id: 5,
+                name: 'Charlie Brown',
+                score: 90,
+            }
+        ];
     }
 }
 
