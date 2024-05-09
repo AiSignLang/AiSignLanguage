9e3f321476f6886f99fe7003e238e67f6044ce39
tsolmonbat-batbold <t.batbold@students.htl-leonding.ac.at>
2024-05-09 17:15:56 +0200

    basic frontend setup

---
+++ frontend/src/Components/Userprofile/User.tsx
@@ -0,0 +1,11 @@
+export default function User(){
+
+
+    return (
+        <div>
+            <h1>Userprofile</h1>
+            <p>Username: TestUser</p>
+            <p>Email:</p>
+        </div>
+    )
+}
