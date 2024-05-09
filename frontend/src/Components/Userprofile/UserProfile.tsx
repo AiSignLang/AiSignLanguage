ad316d79551ff0103f960e9120cc255624a882f6
tsolmonbat-batbold <t.batbold@students.htl-leonding.ac.at>
2024-05-09 19:42:41 +0200

    changes

---
+++ frontend/src/Components/Userprofile/UserProfile.tsx
@@ -0,0 +1,11 @@
+import User from "./User.tsx";
+import Friends from "./Friends.tsx";
+
+export default function UserProfile(){
+    return(
+        <>
+            <User/>
+            <Friends/>
+        </>
+    )
+}
