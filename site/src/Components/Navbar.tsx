2c25998cad2ff104b58598f965ed826dc8fe9cbb
Alexander Resch <alexander@resch-web.at>
2024-05-09 16:06:55 +0200

    nginx config und Ai view überarbeitet

---
+++ site/src/Components/Navbar.tsx
@@ -0,0 +1,15 @@
+﻿import React from "react";
+import {Disclosure, Menu, Transition} from '@headlessui/react'
+import {NavLink} from "react-router-dom";
+
+const Navbar : React.FC = ()=>{
+    return(
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
