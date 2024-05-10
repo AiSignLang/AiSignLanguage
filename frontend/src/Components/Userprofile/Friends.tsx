4e513118be255e8042d320c83c8472f6f58fe4b5
tsolmonbat-batbold <t.batbold@students.htl-leonding.ac.at>
2024-05-10 15:12:46 +0200

    frontend - version - 002 - looks good to me

--- frontend/src/Components/Userprofile/Friends.tsx
+++ frontend/src/Components/Userprofile/Friends.tsx
@@ -3,10 +3,11 @@ import Divider from "@mui/material/Divider";
 export default function Friends(){
     return(
         <div>
-            <Divider style={{backgroundColor:'red'}}></Divider>
+            <Divider style={{backgroundColor:'grey'}}></Divider>
 
-            <h1 className="text-xl font-bold">Friends</h1>
+            <h1 className="text-2xl font-bold pt-2">Friends</h1>
             <p className="text-white">fragezeichen?</p>
+            <div className="bg-green-600">box</div>
             <p>lorem ipsum</p>
         </div>
     )
