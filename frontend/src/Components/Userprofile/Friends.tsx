4a636c3c5296fb15707f6003be774de543dd2d85
tsolmonbat-batbold <t.batbold@students.htl-leonding.ac.at>
2024-05-10 15:07:24 +0200

    frontend - version - 002 - looks good to me

--- frontend/src/Components/Userprofile/Friends.tsx
+++ frontend/src/Components/Userprofile/Friends.tsx
@@ -1,7 +1,11 @@
+import Divider from "@mui/material/Divider";
+
 export default function Friends(){
     return(
         <div>
-            <h1>Friends</h1>
+            <Divider style={{backgroundColor:'red'}}></Divider>
+
+            <h1 className="text-xl font-bold">Friends</h1>
             <p className="text-white">fragezeichen?</p>
             <p>lorem ipsum</p>
         </div>
--- frontend/src/Components/Userprofile/UserProfile.tsx
+++ frontend/src/Components/Userprofile/UserProfile.tsx
@@ -2,28 +2,26 @@ import NavbarProto from "./NavbarProto.tsx";
 import User from "./User.tsx";
 import Friends from "./Friends.tsx";
 
-
 export default function UserProfile(){
     return(
         <>
-            <div className="bg-blue-950 text-gray-100 grid md:grid-cols-3 gap-2">
+            <div className="bg-blue-950 text-gray-100 grid md:grid-cols-3 gap-2 p-8">
                 <div
-                    className="md:col-start-1 md:col-end-2 md:row-start-1 md:row-end-2
-                    sm:col-start-1 sm:col-end-2 sm:row-start-1 sm:row-end-1 bg-green-600"
+                    className="md:col-start-1 md:col-span-2 md:row-start-1 md:row-end-2
+                    sm:col-start-1 sm:col-span-4 sm:row-start-1 sm:row-end-1"
                 >
                     <NavbarProto/>
                 </div>
 
                 <div
-                    className="md:col-start-2 md:col-end-4 md:row-start-1 md:row-end-2
-                    sm:col-start-4 sm:col-end-6 sm:row-start-1 sm:row-end-1 bg-yellow-950"
+                    className="md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-2
+                    sm:col-start-4 sm:col-end-6 sm:row-start-1 sm:row-end-1 text-center m-10"
                 >
                     <User/>
                 </div>
-
                 <div
-                    className="md:col-start-4 md:col-end-6 md:row-start-2 md:row-end-2
-                    sm:col-start-6 sm:col-end-8 sm:row-start-3 sm:row-end-4 bg-red-500"
+                    className="md:col-start-2 md:col-span-1
+                    sm:col-start-3 sm:col-span-3 text-center m-5"
                 >
                     <Friends/>
                 </div>
