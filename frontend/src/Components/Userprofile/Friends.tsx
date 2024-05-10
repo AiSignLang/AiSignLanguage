f1d07bf547e07434a75842565d42bff36172fe0e
tsolmonbat-batbold <t.batbold@students.htl-leonding.ac.at>
2024-05-10 11:54:58 +0200

    frontend - pushing up everything for review

--- frontend/src/Components/Userprofile/Friends.tsx
+++ frontend/src/Components/Userprofile/Friends.tsx
@@ -1,7 +1,9 @@
 export default function Friends(){
     return(
-        <>
-            <h1>User</h1>
-        </>
+        <div>
+            <h1 className="bg-black text-teal-600">Friends</h1>
+            <p className="text-white">fragezeichen?</p>
+            <p>lorem ipsum</p>
+        </div>
     )
 }
--- frontend/src/Components/Userprofile/UserProfile.tsx
+++ frontend/src/Components/Userprofile/UserProfile.tsx
@@ -1,11 +1,33 @@
-
 import NavbarProto from "./NavbarProto.tsx";
+import User from "./User.tsx";
+import Friends from "./Friends.tsx";
+
 
 export default function UserProfile(){
     return(
         <>
-            <NavbarProto></NavbarProto>
+            <div className="flex-wrap bg-blue-950 text-gray-100 grid grid-cols-10 grid-rows-2 gap-2">
+                <div
+                    className="md:col-start-1 md:col-end-2 md:row-start-1 md:row-end-2
+                    xs:col-start-1 xs:col-end-2 xs:row-start-1 xs:row-end-1"
+                >
+                    <NavbarProto/>
+                </div>
 
+                <div
+                    className="md:col-start-4 md:col-end-6 md:row-start-1 md:row-end-2
+                    xs:col-start-6 xs:col-end-8 xs:row-start-1 xs:row-end-2"
+                >
+                    <User/>
+                </div>
+
+                <div
+                    className="md:col-start-4 md:col-end-6 md:row-start-2 md:row-end-2
+                    xs:col-start-6 xs:col-end-8 xs:row-start-2 xs:row-end-2"
+                >
+                    <Friends/>
+                </div>
+            </div>
         </>
     )
 }
