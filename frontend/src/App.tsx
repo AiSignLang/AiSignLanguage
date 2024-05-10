13eb7cbfbf6a5384c5fa254a4b7c5754bff7a385
tsolmonbat-batbold <t.batbold@students.htl-leonding.ac.at>
2024-05-10 11:54:58 +0200

    frontend - pushing up everything for review

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import UserProfile from "./components/userprofile/UserProfile.tsx";
+
+function App() {
+
+  return (
+    <>
+      <UserProfile/>
+    </>
+  )
+}
+
+export default App
