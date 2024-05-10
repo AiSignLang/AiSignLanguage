155c9f9707ffb4cfb9fca88a085aacb78a21bb43
tsolmonbat-batbold <t.batbold@students.htl-leonding.ac.at>
2024-05-10 15:27:54 +0200

    frontend - userprofile - navbar mock + User mock version finalized

--- frontend/src/Components/Userprofile/Friends.tsx
+++ frontend/src/Components/Userprofile/Friends.tsx
@@ -1,13 +1,16 @@
 import Divider from "@mui/material/Divider";
 
 export default function Friends(){
+    
+
     return(
         <div>
             <Divider style={{backgroundColor:'grey'}}></Divider>
 
             <h1 className="text-2xl font-bold pt-2">Friends</h1>
-            <p className="text-white">fragezeichen?</p>
-            <div className="bg-green-600">box</div>
+
+            {/* TODO: Subgrid in here */}
+            <div className="">all the names</div>
         </div>
     )
 }
