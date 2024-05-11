fcdfd5958918b0332c1a9b27bc39ce13f70b6dbe
tsolmonbat-batbold <t.batbold@students.htl-leonding.ac.at>
2024-05-11 20:20:07 +0200

    frontend - userprofile - friends list

--- frontend/src/Components/Userprofile/UserProfile.tsx
+++ frontend/src/Components/Userprofile/UserProfile.tsx
@@ -3,6 +3,7 @@ import User from "./User.tsx";
 import Friends from "./Friends.tsx";
 
 export default function UserProfile(){
+
     return(
         <>
             <div className="bg-blue-950 text-gray-100 grid md:grid-cols-3 gap-2 p-8">
@@ -19,6 +20,12 @@ export default function UserProfile(){
                 >
                     <User/>
                 </div>
+
+                <div className="md:col-start-2 md:col-end-3 md:row-start-2 md:row-end-2
+                    sm:col-start-4 sm:col-end-6 sm:row-start-2 text-center m-6">
+                    Component for system message, chatting, log out, etc.
+                </div>
+
                 <div
                     className="md:col-start-1 md:col-span-4
                     sm:col-start-3 sm:col-span-3 text-center"
