b67f1acbdb37a2c52b94bf465e5e5d6fae6f25e9
Alexander Resch <alexander@resch-web.at>
2024-05-11 21:47:19 +0200

    vite config build dingsdi

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,11 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import {mediapipe} from 'vite-plugin-mediapipe';
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import dotenv from 'dotenv';
+import {mediapipe} from 'vite-plugin-mediapipe'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-      react(),
-      mediapipe()
-  ]
-})
+export default defineConfig(({mode}) => {
+    const envFile = mode === 'development' ? '.env.development' : '.env.production';
+    dotenv.config({path: envFile});
+    return {
+        plugins: [
+            react(),
+            mediapipe()
+        ],
+        build: {
+            outDir: 'build',
+            assetsDir: 'assets',
+            emptyOutDir: true,
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
+        },
+        server: {
+            proxy: {
+                '/api': {
+                    target: process.env.VITE_API_BASE_URL,
+                    changeOrigin: true,
+                },
+            },
+        },
+    };
+});
