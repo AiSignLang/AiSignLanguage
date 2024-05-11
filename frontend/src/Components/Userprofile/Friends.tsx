0861e39c4db6dff0906f822fa80e62f0cd27cc8d
tsolmonbat-batbold <t.batbold@students.htl-leonding.ac.at>
2024-05-11 20:20:07 +0200

    frontend - userprofile - friends list

--- frontend/src/Components/Userprofile/Friends.tsx
+++ frontend/src/Components/Userprofile/Friends.tsx
@@ -1,16 +1,89 @@
-import Divider from "@mui/material/Divider";
 
-export default function Friends(){
-    
+const people = [
+    {
+        name: 'Leslie Alexander',
+        email: 'leslie.alexander@example.com',
+        role: 'Co-Founder / CEO',
+        imageUrl:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        lastSeen: '3h ago',
+        lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+        name: 'Michael Foster',
+        email: 'michael.foster@example.com',
+        role: 'Co-Founder / CTO',
+        imageUrl:
+            'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        lastSeen: '3h ago',
+        lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+        name: 'Dries Vincent',
+        email: 'dries.vincent@example.com',
+        role: 'Business Relations',
+        imageUrl:
+            'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        lastSeen: null,
+    },
+    {
+        name: 'Lindsay Walton',
+        email: 'lindsay.walton@example.com',
+        role: 'Front-end Developer',
+        imageUrl:
+            'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        lastSeen: '3h ago',
+        lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+        name: 'Courtney Henry',
+        email: 'courtney.henry@example.com',
+        role: 'Designer',
+        imageUrl:
+            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        lastSeen: '3h ago',
+        lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+        name: 'Tom Cook',
+        email: 'tom.cook@example.com',
+        role: 'Director of Product',
+        imageUrl:
+            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        lastSeen: null,
+    },
+]
 
-    return(
-        <div>
-            <Divider style={{backgroundColor:'grey'}}></Divider>
+// TODO: Service provides data to this constant
+export default function Friends() {
+    return (
 
-            <h1 className="text-2xl font-bold pt-2">Friends</h1>
+        <ul role="list" className="grid grid-cols-subgrid col-span-8">
 
-            {/* TODO: Subgrid in here */}
-            <div className="">all the names</div>
-        </div>
+            <div className="md:col-start-1 md:col-end-2
+                    sm:col-start-1 sm:col-end-3 text-center m-6">
+            </div>
+
+            <h1 className="text-2xl font-bold
+                  md:col-start-1 md:col-end-3
+                  sm:col-start-1 sm:col-end-3 text-center">Friends</h1>
+
+            <div className="md:col-start-2 md:col-end-3
+                    sm:col-start-1 sm:col-end-3 text-center m-6">
+                {people.map((person) => (
+                    <li key={person.email} className="flex justify-between gap-x-6 py-5 text-gray-300">
+                        <div className="flex min-w-0 gap-x-4">
+                            <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt=""/>
+                            <div className="min-w-0 flex-auto">
+                                <p className="text-sm font-semibold leading-6">{person.name}</p>
+                            </div>
+                        </div>
+                        <p className="hidden shrink-0 sm:flex sm:flex-col sm:items-end text-sm leading-6">{person.role}</p>
+                    </li>
+                ))}
+
+            </div>
+
+        </ul>
     )
 }
