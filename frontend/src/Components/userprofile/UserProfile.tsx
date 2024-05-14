efe1e868e3247c280eeb1bf39853b3c06503ef65
tsolmonbat-batbold <t.batbold@students.htl-leonding.ac.at>
2024-05-14 13:36:15 +0200

    deleting not needed color background

--- frontend/src/Components/userprofile/UserProfile.tsx
+++ frontend/src/Components/userprofile/UserProfile.tsx
@@ -8,15 +8,6 @@ export default function UserProfile(){
         <>
             <div className="bg-blue-950 text-gray-100 grid md:grid-cols-3 gap-2 p-8">
 
-                {/*
-                <div
-                    className="md:col-start-1 md:col-span-2 md:row-start-1 md:row-end-2
-                    sm:col-start-1 sm:col-span-4 sm:row-start-1 sm:row-end-1"
-                >
-                    <NavbarProto/>
-                </div>
-                */}
-
                 <div className="sm:col-start-1 sm:col-end-4 sm:row-start-1">
                     <NavbarProtoAlp/>
                 </div>
@@ -28,14 +19,14 @@ export default function UserProfile(){
                 </div>
 
                 <div className="md:col-start-1 md:col-end-4 md:row-start-3
-                    sm:col-start-1 sm:col-end-4 sm:row-start-3 text-center m-6 bg-red-500">
+                    sm:col-start-1 sm:col-end-4 sm:row-start-3 text-center m-6">
                     Component for system message, chatting, log out, etc.
                 </div>
 
                 <div
                     className="
                     md:col-start-1 md:col-span-4
-                    sm:col-start-1 sm:col-end-4 bg-yellow-500"
+                    sm:col-start-1 sm:col-end-4"
                 >
                     <Friends/>
                 </div>
