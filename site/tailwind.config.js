60e4aadd5b9ae1e508d31f05c48a1adf69a5447f
elija-lambourne <elija@lambourne.at>
2024-05-15 14:52:35 +0200

    Added custom colour palette (see tailwind.config.js)

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -19,6 +19,10 @@ module.exports = {
         },
         colors: {
             'blue': '#243c5a',
+            'bg-primary': '#0D141A',
+            'bg-secondary': '#1C2B36',
+            'primary': '#0084FF',
+            'text-primary': '#FFFFFF',
             },
     },
     plugins: [],
