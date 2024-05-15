25b5c8181d24bf118e178f580109e9f9ce127312
elija-lambourne <elija@lambourne.at>
2024-05-15 14:24:17 +0200

    Added XnotFound.tsx error page and Unauthorized.tsx error page

--- site/src/Components/userprofile/UserProfile.tsx
+++ site/src/Components/userprofile/UserProfile.tsx
@@ -1,36 +1,13 @@
 import User from "./User.tsx";
 import Friends from "./Friends.tsx";
-import Navbar from "../navbar/Navbar.tsx";
 
 export default function UserProfile(){
 
     return(
         <>
-            <div className="bg-blue-950 text-gray-100 grid md:grid-cols-3 gap-2 p-8">
-
-                <div className="sm:col-start-1 sm:col-end-4 sm:row-start-1">
-                    <Navbar/>
-                </div>
-
-                <div
-                    className="text-center m-6 mt-10
-                    md:col-start-1 md:col-end-4 md:row-start-2
-                    sm:col-start-1 sm:col-end-4 sm:row-start-2">
-                    <User/>
-                </div>
-
-                <div className="md:col-start-1 md:col-end-4 md:row-start-3
-                    sm:col-start-1 sm:col-end-4 sm:row-start-3 text-center m-6">
-                    Component for system message, chatting, log out, etc.
-                </div>
-
-                <div
-                    className="
-                    md:col-start-1 md:col-span-4
-                    sm:col-start-1 sm:col-end-4"
-                >
-                    <Friends/>
-                </div>
+            <div className="bg-blue-950 text-gray-100 p-8">
+                <User></User>
+                <Friends></Friends>
             </div>
         </>
     )
