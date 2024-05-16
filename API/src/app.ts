21153ba090ad2c6230b0675d2c782a8161d2623d
Alexander Resch <alexander@resch-web.at>
2024-05-16 11:48:18 +0200

    init

---
+++ API/src/app.ts
@@ -0,0 +1,12 @@
+﻿import express from "express";
+import { User  } from "./models/User";
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+
+
+
+app.listen(3000, () => {
+    console.log("Server is running at http://localhost:3000");
+});
---
+++ API/src/models/Score.ts
@@ -0,0 +1,29 @@
+﻿import {DataTypes, ForeignKey, Model, Sequelize} from "sequelize";
+import {sequelize} from "../database";
+import {Users} from "./User";
+
+export class Scores extends Model {
+    
+    declare dailyStreak: string;
+    declare allTimeCorrect: number;
+    declare perfectlyDone : number;
+    declare scoreId: string;
+    declare ownerId: ForeignKey<Users>
+    
+    public static Init = () => {
+        Scores.init({
+            scoreId: {
+                type: DataTypes.UUID,
+                primaryKey: true,
+                allowNull: false,
+                defaultValue: DataTypes.UUIDV4
+            }
+
+        }, {
+            sequelize,
+            modelName: 'Score'
+        });
+    }
+}
+
+Scores.belongsTo(Users)
