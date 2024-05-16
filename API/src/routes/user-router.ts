2df13e3ed128df14cd0896981488a79625ea3976
tsolmonbat-batbold <t.batbold@students.htl-leonding.ac.at>
2024-05-16 14:06:08 +0200

    user router creation + first potential route done

---
+++ API/src/routes/user-router.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import {sequelize} from "../database";
+import {Users} from "../models/User";
+import {StatusCodes} from "http-status-codes";
+
+
+const userRouter = express.Router();
+
+
+userRouter.get("/:id", async (req, res) => {
+
+    const id = parseInt(req.params.id, 10);
+    const transaction = await sequelize.transaction();
+
+    try{
+        const user = await Users.findByPk(id);
+
+        if(user === null) {
+            res.sendStatus(StatusCodes.NOT_FOUND);
+            return;
+        }
+
+        res.json(user);
+
+    }catch (err){
+        res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+        await transaction.rollback();
+    }
+
+
+});
