636e04071db1003e155e7d6f29aea34c3cd8c738
elija-lambourne <elija@lambourne.at>
2024-05-18 20:37:59 +0200

    Added add friend button, but it is slighty shifted down for some reason

--- site/src/Components/userprofile/UserProfile.tsx
+++ site/src/Components/userprofile/UserProfile.tsx
@@ -6,11 +6,11 @@ export default function UserProfile(){
 
     return(
         <>
-            <div className="bg-bg-primary text-text-primary p-8 h-full min-h-screen w-full flex p-10">
+            <div className="bg-bg-primary text-text-primary h-full min-h-screen w-full flex p-10">
                 <div className="w-2/6 c-lg:w-1/6 c-md:w-0 c-sm:w-0 xs:w-0 ">
                 </div>
-                <div className="w-3/6 flex c-md:w-4/6 xs:w-full">
-                    <div className="w-full">
+                <div className="w-3/6 flex c-md:w-4/6 xs:w-full h-fit mr-10">
+                    <div className="w-full bg-bg-secondary rounded-3xl p-10">
                         <User username="johndoe"></User>
                         <Friends></Friends>
                     </div>
