48259d50a551542cfb59c7b58a7b4e3e5df5b317
elija-lambourne <elija@lambourne.at>
2024-05-23 12:14:51 +0200

    something

--- site/src/Components/userprofile/UserProfile.tsx
+++ site/src/Components/userprofile/UserProfile.tsx
@@ -1,12 +1,16 @@
 import User from "./User.tsx";
 import Friends from "./Friends.tsx";
 import {Recommendations} from "./Recommendations.tsx";
+import Navbar from "../navbar/Navbar.tsx";
 
 export default function UserProfile(){
 
     return(
         <>
-            <div className="bg-bg-primary text-text-primary h-full min-h-screen w-full flex p-10">
+            <div>
+                <Navbar></Navbar>
+                <div className="bg-bg-primary text-text-primary h-full min-h-screen w-full flex p-10">
+
                 <div className="w-2/6 c-lg:w-1/6 c-md:w-0 c-sm:w-0 xs:w-0 ">
                 </div>
                 <div className="w-3/6 flex c-md:w-4/6 xs:w-full h-fit mr-10">
@@ -18,6 +22,7 @@ export default function UserProfile(){
                 <div className="w-1/6 xs:w-0">
                     <Recommendations></Recommendations>
                 </div>
+            </div>
             </div>
         </>
     )
