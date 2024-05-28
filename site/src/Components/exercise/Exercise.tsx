4abfc8f80033b5b2e3ad39f40e90190db28718c9
elija-lambourne <elija@lambourne.at>
2024-05-28 21:53:25 +0200

    Flowbite tooltips aren't working rn

--- site/src/Components/exercise/Exercise.tsx
+++ site/src/Components/exercise/Exercise.tsx
@@ -13,6 +13,10 @@ export function Exercise(props: IProps) {
     const queryParams = new URLSearchParams(location.search);
     const type = queryParams.get('type');  // replace 'myParam' with the name of your parameter
     const [level, setLevel] = useState<ILevel | null>(null);
+    const [currentTaskIndex, setCurrentTaskIndex] = useState(0);
+    const handleNextTask = () => {
+        setCurrentTaskIndex(prevIndex => prevIndex + 1);
+    };
 
     useEffect(() => {
         switch (type) {
@@ -31,14 +35,24 @@ export function Exercise(props: IProps) {
         <div className="bg-bg-primary text-text-primary h-full min-h-screen w-full">
             <Navbar></Navbar>
             <p>
-                {level ? level.tasks.map(task => {
-                    return (
-                        <div key={task.taskID}>
-                            <p></p>
-                        </div>
-                    );
-                }) : 'No tasks found'}
+                {level && level.tasks[currentTaskIndex] ? (
+                    <div className="bg-bg-secondary rounded-2xl p-4 w-1/3 mt-20" key={level.tasks[currentTaskIndex].taskID}>
+                        {level.tasks[currentTaskIndex].taskData.map((data, index) => (
+                            <>
+                                <span><span className="underline" data-tooltip-target={`tooltip-${index}`} key={index}>{data}</span> </span>
+
+                                <div id={`tooltip-${index}`} role="tooltip"
+                                     className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                                    Tooltip content
+                                    <div className="tooltip-arrow" data-popper-arrow></div>
+                                </div>
+                            </>
+                        ))}
+                    </div>
+                ) : 'No tasks found'}
             </p>
+            <button onClick={handleNextTask}>Next Task</button>
+
         </div>
     );
 };
