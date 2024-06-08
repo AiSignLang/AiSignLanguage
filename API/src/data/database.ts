554cb4c874aef82cee08637313ec4912663fa051
Alexander Resch <alexander@resch-web.at>
2024-06-08 13:46:48 +0200

    es wreden 1000_0000 tausend mehr commits sein, bitte nicht sterben

--- API/src/data/database.ts
+++
@@ -1,9 +0,0 @@
-﻿import {Sequelize} from "sequelize-typescript";
-
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: 'data/db.sqlite3',
-    models: [__dirname+"/models/*.ts"],
-    logging: false
-});
-export default sequelize;
