c276f5ed6289aab5882b40bf76dbd008c2f97eba
Alexander Resch <alexander@resch-web.at>
2024-06-08 16:44:20 +0200

    implemented authentication

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import config from "./config.ts";
 
-export const PORT: number = 8080;
-export const EXTERNAL_PORT: number = 8080;
-
-export const DOMAIN: string = "http://localhost";
-export let EXTERNAL_DOMAIN: string = "http://localhost";
-
-if (process.env.NODE_ENV === "publish") {
-    EXTERNAL_DOMAIN = "http://asl.lambourne.at";
-}
-
-export const ADDRESS: string = `${DOMAIN}:${PORT}`;
-export const EXTERNAL_ADDRESS: string = `${EXTERNAL_DOMAIN}:${EXTERNAL_PORT}`;
-
-console.log('server running on: ', ADDRESS);
-console.log('server running external on: ', EXTERNAL_ADDRESS);
+console.log('server running on: ', config.address);
+console.log('server running external on: ', config.externalAddress);
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
