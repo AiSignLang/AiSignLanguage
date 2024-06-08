876807bbd1c393f9f9fa2548fd3a775b4e4364bb
Alexander Resch <alexander@resch-web.at>
2024-06-08 14:02:48 +0200

    site war weg huch? haha

---
+++ site/src/services/auth/google-auth-service.ts
@@ -0,0 +1,40 @@
+import {EXTERNAL_ADDRESS} from "../../main.tsx";
+import {navigate} from "../../model/Utils.ts";
+import {fetchRestEndpoint} from "../../support/FetchEndpoint.ts";
+import OAuthGoogleUserData from "../../model/auth/OAuthGoogleUserData.ts";
+import {Credentials} from "google-auth-library";
+
+
+
+export async function googleAuth() {
+    const response = await fetch(`${EXTERNAL_ADDRESS}/auth/google-auth/request`, {
+        method: "POST",
+    });
+    const data = await response.json();
+    navigate(data.url);
+}
+
+export async function getUserData(access_token: string): Promise<OAuthGoogleUserData|undefined> {
+    return await fetchRestEndpoint<OAuthGoogleUserData>(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`, "GET")
+}
+
+export async function refreshToken(refresh_token: string): Promise<void> {
+    const data = await fetchRestEndpoint<Credentials>(`${EXTERNAL_ADDRESS}/auth/google-auth/refresh?refresh_token=${refresh_token}`, "GET");
+    if (!data || !data.access_token || !data.refresh_token) {
+        navigate("/Unauthorized");
+    }
+    sessionStorage.setItem('access_token', data!.access_token!);
+    localStorage.setItem('refresh_token', data!.refresh_token!);
+
+}
+
+export async function verifyGoogleAuth(access_token: string): Promise<boolean> {
+    const response = await fetch(`${EXTERNAL_ADDRESS}/auth/google-auth/verify`, {
+        method: "POST",
+        body: JSON.stringify({access_token}),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    return response.ok;
+}
