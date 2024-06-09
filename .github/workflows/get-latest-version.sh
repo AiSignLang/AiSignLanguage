87314138a15acf44d6bc4ec2895c66604b2a5a61
Alexander Resch <alexander@resch-web.at>
2024-06-09 18:40:55 +0200

    Add files via upload

---
+++ .github/workflows/get-latest-version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set your Docker Hub username and the name of your image
+DOCKER_HUB_USERNAME="ital3x"
+DOCKER_IMAGE_NAME=$1
+
+# Get the JSON response from Docker Hub
+JSON_RESPONSE=$(curl -s "https://hub.docker.com/v2/namespaces/${DOCKER_HUB_USERNAME}/repositories/${DOCKER_IMAGE_NAME}/tags")
+
+
+# Parse the JSON response to get the "name" field
+VERSION=$(echo "$JSON_RESPONSE" | jq -r '.results[0].name')
+
+# Split the "name" field by the "." character and get the last number
+LAST_NUMBER=$(echo "$VERSION" | awk -F. '{print $NF}')
+
+# Increment the last number
+NEW_VERSION=$((LAST_NUMBER + 1))
+
+echo "NEW_VERSION=$NEW_VERSION" >> $GITHUB_ENV
+
