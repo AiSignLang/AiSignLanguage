4e70f58529cc888650498b46efa89b61f0c657e6
tsolmonbat-batbold <t.batbold@students.htl-leonding.ac.at>
2024-06-11 21:13:42 +0200

    made landing page

---
+++ site/src/Components/landingPage/subcomponents/Header.tsx
@@ -0,0 +1,30 @@
+
+export default function Header() {
+
+    return (
+        <div className="bg-white">
+
+            <div className="relative isolate px-6 pt-14 lg:px-8">
+                <div className="mx-auto max-w-2xl py-24">
+
+                    <div className="text-center">
+                        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                            Interested in Sign Language?
+                        </h1>
+                        <p className="mt-6 text-lg leading-8 text-gray-600">
+                            Learn it with our well made project - AiSL
+                        </p>
+                        <div className="mt-10 flex items-center justify-center gap-x-6">
+                            <a
+                                href="https://github.com/AiSignLang/AiSignLanguage/tree/main/Docs"
+                                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                 github organization <span aria-hidden="true">→</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
