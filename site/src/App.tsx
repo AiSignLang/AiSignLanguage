6efc818788fdb1eec33dd58e2575b748e51aa557
tsolmonbat-batbold <t.batbold@students.htl-leonding.ac.at>
2024-06-11 21:13:42 +0200

    made landing page

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -12,16 +12,16 @@ import {XnotFound} from "./Components/errors/XnotFound.tsx";
 import {Course} from "./Components/course/Course.tsx";
 import {Sandbox} from "./Components/sandbox/Sandbox.tsx";
 import {Exercise} from "./Components/exercise/Exercise.tsx";
-import {SignIn} from "./Components/welcome/SignIn.tsx";
 import TrainingRead from "./Components/training/TrainingRead.tsx";
 import TrainingSign from "./Components/training/TrainingSign.tsx";
+import LandingPage from "./Components/landingPage/LandingPage.tsx";
 function App() {
     
   return (
       <Router>
         <Routes>
             {/*   <Route path="/" element={<XnotFound subject={"Profile"} message={"Please try again later."}/>}/> */}
-            <Route path="/" element={<SignIn/>}/>
+            <Route path="/" element={<LandingPage/>}/>
             <Route path="/course" element={<Course courseID="next"/>}/>
             <Route path="/sandbox" element={<Sandbox/>}/>
             <Route path="/exercise" element={<Exercise/>}/>
