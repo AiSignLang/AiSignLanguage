597851fbe6cd8a2b16ac590d81bf85dcad75781b
Alexander Resch <alexander@resch-web.at>
2024-06-12 15:54:00 +0200

    ?

--- .github/workflows/get-latest-version.sh
+++ .github/workflows/get-latest-version.sh
@@ -7,6 +7,9 @@ DOCKER_IMAGE_NAME=$1
 # Get the JSON response from Docker Hub
 JSON_RESPONSE=$(curl -s "https://hub.docker.com/v2/namespaces/${DOCKER_HUB_USERNAME}/repositories/${DOCKER_IMAGE_NAME}/tags")
 
+apt update
+apt install jq
+apt install awk
 
 # Parse the JSON response to get the "name" field
 VERSION=$(echo "$JSON_RESPONSE" | jq -r '.results[0].name')
@@ -18,4 +21,5 @@ LAST_NUMBER=$(echo "$VERSION" | awk -F. '{print $NF}')
 NEW_VERSION=$((LAST_NUMBER + 1))
 
 echo "NEW_VERSION=$NEW_VERSION" >> $GITHUB_ENV
+echo "NEW_VERSION=$NEW_VERSION"
 
