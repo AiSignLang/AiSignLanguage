5b7bf1983c8864299ffee78e8e7b56abb6d0b532
Alexander Resch <alexander@resch-web.at>
2024-06-14 17:58:31 +0200

    some performance fixes

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -5,7 +5,7 @@ import {
     Route,
 } from "react-router-dom";
 
-import './App.css'
+import './dist/tailwind.css'
 import UserProfile from "./Components/userprofile/UserProfile.tsx";
 import {XnotFound} from "./Components/errors/XnotFound.tsx";
 import {Course} from "./Components/course/Course.tsx";
@@ -14,6 +14,12 @@ import {Exercise} from "./Components/exercise/Exercise.tsx";
 import TrainingRead from "./Components/training/TrainingRead.tsx";
 import TrainingSign from "./Components/training/TrainingSign.tsx";
 import LandingPage from "./Components/landingPage/LandingPage.tsx";
+import {SignIn} from "./Components/welcome/SignIn.tsx";
+import {SignUp} from "./Components/welcome/SignUp.tsx";
+import {Unauthorized} from "./Components/errors/Unauthorized.tsx";
+import OAuthRedirect from "./Components/auth/OAuthRedirect.tsx";
+
+
 function App() {
     
   return (
@@ -21,15 +27,18 @@ function App() {
         <Routes>
             {/*   <Route path="/" element={<XnotFound subject={"Profile"} message={"Please try again later."}/>}/> */}
             <Route path="/" element={<LandingPage/>}/>
+            <Route path="/login" element={<SignIn/>}/>
+            <Route path="/signup" element={<SignUp/>}/>
             <Route path="/course" element={<Course courseID="next"/>}/>
             <Route path="/sandbox" element={<Sandbox/>}/>
             <Route path="/exercise" element={<Exercise/>}/>
             <Route path="/training-read" element={<TrainingRead/>}/>
             <Route path="/training-sign" element={<TrainingSign/>}/>
             <Route path="*" element={<XnotFound subject="Page"/>}/>
+            <Route path="/Unauthorized" element={<Unauthorized />}/>
             <Route path="/ProfileNotFound" element={<XnotFound subject="Profile" />}/>
             <Route path="/profile" element={<UserProfile/>}/>
-            <Route path="/login" element={<LoginForm/>}/>
+            <Route path="/oauth-google-redirect" element={<OAuthRedirect/>}/>
         </Routes>
       </Router>
   );
--- site/src/Components/userprofile/UserProfile.tsx
+++ site/src/Components/userprofile/UserProfile.tsx
@@ -4,10 +4,17 @@ import {Recommendations} from "./Recommendations.tsx";
 import Navbar from "../navbar/Navbar.tsx";
 import {useLocation} from "react-router-dom";
 import {NavService} from "../../services/NavigationService.ts";
+import {navigate} from "../../model/Utils.ts";
+
 
 export default function UserProfile(){
     NavService.changeNavHighlight(useLocation().pathname);
-
+    
+    if (sessionStorage.getItem('access_token') === null
+        || sessionStorage.getItem("username") === null) {
+        navigate('/login');
+        return ;
+    }
     return(
         <>
             <Navbar></Navbar>
@@ -21,11 +28,14 @@ export default function UserProfile(){
 
                 <div className="w-1/2 flex c-md:w-4/6 xs:w-full h-fit mr-10">
                     <div className="w-full bg-bg-secondary rounded-3xl p-10">
-                        <User username="johndoe"/>
+                        <User username={sessionStorage.getItem("username")??"John Doe" }/>
                         <Friends/>
                     </div>
                 </div>
 
+                {/*<div className="w-1/6 xs:w-0">
+                    <Recommendations/>
+                </div>*/}
             </div>
         </>
     )
