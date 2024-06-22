b2e10d5b6139d101cab072de4f78000db45a944c
Alexander Resch <alexander@resch-web.at>
2024-06-22 23:00:19 +0200

    why

--- API/src/app.ts
+++ API/src/app.ts
@@ -7,11 +7,12 @@ import {friendRouter} from "./routes/friend-router";
 import googleAuthRouter from "./routes/auth/google-auth-router";
 import * as fs from "node:fs";
 import config from "./config";
-configDotenv()
+
 import cors from "cors";
 import aslAuthRouter from "./routes/auth/asl-auth-router";
 import {configDotenv} from "dotenv";
 export const app = express();
+configDotenv()
 
 if(!process.env.ASL_SECRET ||
     !process.env.GOOGLE_CLIENT_ID ||
