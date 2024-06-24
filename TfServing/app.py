f7bf35dbb3e1e20ac82497336dc1b0546c706e67
elija-lambourne <elija@lambourne.at>
2024-06-24 18:16:10 +0200

    Switched back to flask

--- TfServing/app.py
+++ TfServing/app.py
@@ -2,23 +2,26 @@ import json
 import tensorflow as tf
 import numpy as np
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 
 # Load the .keras model
 model = tf.keras.models.load_model('dynamicModel.keras')
 actions = ['hallo', 'wiegehtesdir', 'tschüss', 'ich', 'du', 'wir']
-
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         data = request.get_json()
-        print(f"Received request: {data}")
+        print(f"Received request: {len(data)}")
         if 'instances' not in data:
             return jsonify({'error': 'no instances key found'}), 400
 
         data_reshaped = np.reshape(data['instances'], (-1, 30, 1662))
+        print(f"Data reshaped: {len(data_reshaped.shape)}")
         input_tensor = tf.convert_to_tensor(data_reshaped)
         # Print input shape
         print(f"Input shape: {input_tensor.shape}")
