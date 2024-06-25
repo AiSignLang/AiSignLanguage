67b88ad300b47a087b4445c025d30713e332921e
Tsolmonbat Batbold <113517348+tsolmonbat-batbold@users.noreply.github.com>
2024-06-25 02:02:24 +0200

    corrected service

--- site/src/services/MistakeService.ts
+++ site/src/services/MistakeService.ts
@@ -1,10 +1,11 @@
 import {IMistake} from "../model/backend/IMistake.ts";
 import {fetchRestEndpoint} from "../support/FetchEndpoint.ts";
+import config from "../config.ts";
 
 class MistakeService {
     public async getAll(): Promise<IMistake[]>{
         try{
-            const mistakes = await fetchRestEndpoint<IMistake[]>("/api/mistakes", "GET");
+            const mistakes = await fetchRestEndpoint<IMistake[]>(`${config.externalAddress}/api/mistakes`, "GET");
             return mistakes ? mistakes : [];
         }catch (e){
             console.error(e);
--- site/src/services/TaskService.ts
+++ site/src/services/TaskService.ts
@@ -1,11 +1,12 @@
 import {ITask} from "../model/backend/ITask.ts";
 import {fetchRestEndpoint} from "../support/FetchEndpoint.ts";
 import {IMistake} from "../model/backend/IMistake.ts";
+import config from "../config.ts";
 
 class TaskService {
     public async getPer(mistake: IMistake): Promise<ITask | null>{
         try{
-            const task = await fetchRestEndpoint<ITask>(`/api/tasks/task/${mistake.taskId}`, "GET");
+            const task = await fetchRestEndpoint<ITask>(`${config.externalAddress}/api/tasks/task/${mistake.taskId}`, "GET");
             return task ? task : null;
         }catch (e){
             console.error(e);
